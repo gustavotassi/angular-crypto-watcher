666d8838425d88d46f1e6b82367f4645adf38113
lucastso <lucastassisouza@gmail.com>
2021-04-17 19:47:02 -0300

    safe commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
-interface Client {
-  id: string;
-  nome: string;
-  idade: number;
-  createdAt: string;
-  isDeleted: boolean;
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  title = 'angular-crypto-watcher';
-  public nome = 'lucas';
-
-  constructor() {}
-
-  ngOnInit(): void {
-    console.log(this.nome);
-  }
+export class AppComponent {
+  isCollapsed = false;
 }
